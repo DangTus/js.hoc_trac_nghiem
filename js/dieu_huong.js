d4a3ea54d38c046396ce259f5187a4f77832ccb1
DangTus <dangtu9520@gmail.com>
2023-01-01 15:22:50 +0700

    Tus fix loi link chuyen trang lan 4

--- js/dieu_huong.js
+++ js/dieu_huong.js
@@ -1,10 +1,10 @@
 // var link_web = '.'
-var link_web = './js.hoc_trac_nghiem'
+// var link_web = './js.hoc_trac_nghiem'
 
 function sang_trang_hoc() {
-    location.href = link_web + '/page/hoc.html'
+    location.href = "./page/hoc.html"
 }
 
 function ve_trang_chu() {
-    location.href = link_web
+    location.href = "./"
 }
