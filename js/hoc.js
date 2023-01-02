384f293218127df270cce82a7ba9ccac12270a69
DangTus <dangtu9520@gmail.com>
2023-01-02 07:39:06 +0700

    Tus hoan thanh sang cau tiep theo, bat su kien khi het cau hoi

--- js/hoc.js
+++ js/hoc.js
@@ -1,19 +1,18 @@
-var noi_dung_cau_hoi_html = document
+const noi_dung_cau_hoi_html = document
     .getElementById("cau_hoi")
     .getElementsByTagName("span")[0];
-var noi_dung_dap_an_html = document
+const noi_dung_dap_an_html = document
     .getElementById("dap_an")
     .getElementsByTagName("span");
-var abcd_html = document
+const abcd_html = document
     .getElementById("dap_an")
     .getElementsByTagName("strong");
-var abcd = ["A. ", "B. ", "C. ", "D. "];
-var dap_an_da_chon = null
-var dap_an_dung = null
+const abcd = ["A. ", "B. ", "C. ", "D. "];
 
-function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-}
+var dap_an_da_chon = null;
+var dap_an_dung = null;
+var bo_cau_hoi = null;
+var cau_hoi_hien_tai = -1;
 
 // sự kiện khi bắt đầu load trang
 document.getElementsByTagName("body")[0].onload = function () {
@@ -27,68 +26,114 @@ document.getElementsByTagName("body")[0].onload = function () {
             dataArray = JSON.parse(data_result).data;
 
             //xáo trộn câu hỏi
-            dataArrayRand = shuffle(dataArray);
+            bo_cau_hoi = shuffle(dataArray);
 
-            //lấy nội câu hỏi và câu trả lời
-            cau_hoi = dataArrayRand[0].cau_hoi;
-            dap_an = dataArrayRand[0].dap_an;
-            dap_an_dung = dataArrayRand[0].dap_an_dung;
-
-            //đổ câu hỏi và các câu trả lời ra web
-            noi_dung_cau_hoi_html.innerHTML = cau_hoi;
-            for (i = 0; i < 4; i++) {
-                abcd_html[i].innerHTML = abcd[i];
-                noi_dung_dap_an_html[i].innerHTML = dap_an[i];
-            }
-
-            hide_load();
+            show_cau_hoi();
         });
 };
 
+function show_cau_hoi() {
+    show_load();
+    reset_css_item_dap_an();
+    show_btn_tra_loi();
+
+    cau_hoi_hien_tai += 1;
+
+    //kiểm tra xem còn câu hỏi hay không, nếu không còn thì về trang chủ
+    if (cau_hoi_hien_tai < bo_cau_hoi.length) {
+        //lấy nội câu hỏi, câu trả lời và đáp án
+        cau_hoi = bo_cau_hoi[cau_hoi_hien_tai].cau_hoi;
+        dap_an = bo_cau_hoi[cau_hoi_hien_tai].dap_an;
+        dap_an_dung = bo_cau_hoi[cau_hoi_hien_tai].dap_an_dung;
+
+        //đổ câu hỏi và các câu trả lời ra web
+        noi_dung_cau_hoi_html.innerHTML = cau_hoi;
+        for (i = 0; i < 4; i++) {
+            abcd_html[i].innerHTML = abcd[i];
+            noi_dung_dap_an_html[i].innerText = dap_an[i];
+        }
+    } else {
+        alert("Hết câu hỏi rồi");
+        ve_trang_chu();
+    }
+
+    hide_load();
+}
+
 function click_dap_an(dap_an_da_click) {
-    reset_css_item_dap_an()
+    reset_css_item_dap_an();
 
     dap_an_da_click_html = document.getElementById("dap_an_" + dap_an_da_click);
-    dap_an_da_click_html.classList.add('item-select')
+    dap_an_da_click_html.classList.add("item-select");
 
-    dap_an_da_chon = dap_an_da_click
+    dap_an_da_chon = dap_an_da_click;
 }
 
 function kiem_tra_dap_an() {
-    if(dap_an_da_chon) {
-        item_dap_an = document.getElementById('dap_an_'+dap_an_da_chon)
-        noi_dung_dap_an = item_dap_an.getElementsByTagName('span')[0].innerText
-        if(noi_dung_dap_an == dap_an_dung) {
-            item_dap_an.classList.add('item-right')
+    if (dap_an_da_chon) {
+        item_dap_an = document.getElementById("dap_an_" + dap_an_da_chon);
+        noi_dung_dap_an = item_dap_an.getElementsByTagName("span")[0].innerText;
+
+        if (noi_dung_dap_an == dap_an_dung) {
+            item_dap_an.classList.add("item-right");
         } else {
-            item_dap_an.classList.add('item-wrong')
-            show_dap_an_dung()
+            item_dap_an.classList.add("item-wrong");
+            show_dap_an_dung();
         }
+
+        show_btn_cau_tiep_theo();
     } else {
-        alert('Vui lòng chọn đáp án')
+        alert("Vui lòng chọn đáp án");
     }
 }
 
 function reset_css_item_dap_an() {
-    dap_an_html = document.getElementById("dap_an").getElementsByClassName('item');
-    dap_an_html_len = dap_an_html.length;
+    dap_an_html = document
+        .getElementById("dap_an")
+        .getElementsByClassName("item");
 
+    dap_an_html_len = dap_an_html.length;
     for (i = 0; i < dap_an_html_len; i++) {
-        dap_an_html[i].className = "item"
+        dap_an_html[i].className = "item";
     }
 }
 
 function show_dap_an_dung() {
-    dap_an_html = document.getElementById("dap_an").getElementsByClassName('item');
+    dap_an_html = document
+        .getElementById("dap_an")
+        .getElementsByClassName("item");
+
     dap_an_html_len = dap_an_html.length;
-
     for (i = 0; i < dap_an_html_len; i++) {
-        noi_dung_dap_an = dap_an_html[i].getElementsByTagName('span')[0].innerText
+        noi_dung_dap_an =
+            dap_an_html[i].getElementsByTagName("span")[0].innerText;
 
-        if(noi_dung_dap_an == dap_an_dung) {
-            dap_an_html[i].classList.add('item-right')
+        if (noi_dung_dap_an == dap_an_dung) {
+            dap_an_html[i].classList.add("item-right");
+            return;
         }
     }
+}
+
+function show_btn_cau_tiep_theo() {
+    btn_tra_loi = document.getElementById("btn_tra_loi");
+    btn_cau_tiep_theo = document.getElementById("btn_cau_tiep_theo");
+
+    btn_tra_loi.classList.add("hidden");
+    btn_cau_tiep_theo.classList.remove("hidden");
+}
+
+function show_btn_tra_loi() {
+    btn_tra_loi = document.getElementById("btn_tra_loi");
+    btn_cau_tiep_theo = document.getElementById("btn_cau_tiep_theo");
+
+    btn_tra_loi.classList.remove("hidden");
+    btn_cau_tiep_theo.classList.add("hidden");
+}
+
+// đây là hàm dùng để xáo trộn các phần tử trong mảng
+function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
 }
 
 function show_load() {
